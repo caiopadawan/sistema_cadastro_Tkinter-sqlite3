43ac271340766f5c112222f226d42f0fd287c7fe
caiopadawan <caiobrone77@gmail.com>
2020-06-29 00:53:42 -0300

    add titulo e resizable para a window

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tkinter import *
 
 application = Tk()
+application.title('Sistema de Cadasrto')
 application.geometry("640x480+100+100")
 application['bg'] = 'red'
+application.resizable(False, False)
 application.mainloop()
