d81d8dad0cd0e264f56d071ec36b24125d448609
caiopadawan <caiobrone77@gmail.com>
2020-06-29 13:36:54 -0300

    criada uma instancia de label e aplicada formatações

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ application.title('Sistema de Cadasrto')
 application.geometry("640x480+100+100")
 application['bg'] = 'gray'
 application.resizable(False, False)
+lb_1 = Label(application,
+			text='Inserindo uma Label',
+			background='green',
+			fg='red',
+			font='Arial 20',
+			width=30)
 btn_1 = Button(application,
 				text='OK',
 				background='black',
@@ -12,6 +18,7 @@ btn_1 = Button(application,
 				width=30,
 				font='Arial 18',
 				anchor=W)
+lb_1.pack()
 btn_1.pack()
 
 application.mainloop()
