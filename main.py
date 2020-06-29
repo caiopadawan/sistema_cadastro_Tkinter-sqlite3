68470c5c2cc96866e834c7b105ccb50ebcbac156
caiopadawan <caiobrone77@gmail.com>
2020-06-29 01:45:35 -0300

    Alterada a cor de fundo, e add novas configurações ao botão btn_1

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from tkinter import *
 application = Tk()
 application.title('Sistema de Cadasrto')
 application.geometry("640x480+100+100")
-application['bg'] = 'red'
+application['bg'] = 'gray'
 application.resizable(False, False)
 btn_1 = Button(application,
 				text='OK',
 				background='black',
 				fg='white',
-				width=30)
-				
+				width=30,
+				font='Arial 18',
+				anchor=W)
 btn_1.pack()
 
 application.mainloop()
