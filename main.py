b463f95e09ed652ef1e5ff7276a4c2e1ac3177a9
caiopadawan <caiobrone77@gmail.com>
2020-06-29 01:20:35 -0300

    Criado uma instancia de botão chamada btn_1, e aplicada algumas configurações

--- main.py
+++ main.py
@@ -5,4 +5,14 @@ application.title('Sistema de Cadasrto')
 application.geometry("640x480+100+100")
 application['bg'] = 'red'
 application.resizable(False, False)
+btn_1 = Button(application,
+				text='OK',
+				background='black',
+				fg='white',
+				width=30)
+				
+btn_1.pack()
+
 application.mainloop()
+
+
